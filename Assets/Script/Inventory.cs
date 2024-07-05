064a1dcfd2f1695c4b65282b5bfd28b0fe781120
Giuanix <giovannimascaro88@gmail.com>
2024-07-05 19:19:29 +0200

    InizioBarraTimer

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public int NumeroFrutti;
+    public GameObject MinigiocoCasse;
     [SerializeField] private int TotNumeroFrutti;
+
+    void Start()
+    {
+        NumeroFrutti = 0;
+        MinigiocoCasse.SetActive(true);
+    }
     void Update()
     {
-        if (NumeroFrutti == TotNumeroFrutti);
+        if (NumeroFrutti == TotNumeroFrutti)
         {
-            Debug.Log("LIVELLO COMPLETATO");
+            MinigiocoCasse.SetActive(false);
         }  
     }
 }
---
+++ Assets/Script/TimerMinigiocoCasse.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerMinigiocoCasse : MonoBehaviour
+{
+    public Image BarraTimerMinigiocoCassa;
+
+    public float TimerMinigiocoCassa;
+    public float MinTimerTimerMinigiocoCassa;
+    public float MaxTimerTimerMinigiocoCassa;
+}

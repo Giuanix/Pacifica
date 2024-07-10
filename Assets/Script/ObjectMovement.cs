1cca61c546b2c7333d731a68c68ac85ca8dd0548
Giuanix <giovannimascaro88@gmail.com>
2024-07-10 13:35:54 +0200

    DifficoltàProgressiva

--- Assets/Script/ObjectMovement.cs
+++ Assets/Script/ObjectMovement.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 public class ObjectMovement : MonoBehaviour
 {
+    public float SpeedObject = -1f;
+    private float Timer;
+    [SerializeField] private float MaxTimer;
+    public SlotSalvataggioSpeedObject Manager;
+    void Start()
+    {
+        GameObject.Find("GameManagerMinigiocoCaramelle").GetComponent<SlotSalvataggioSpeedObject>();
+        Timer = MaxTimer;
+        SpeedObject = Manager.SpeedObjectModificato;
+    }
     void Update()
     {
-        transform.position += new Vector3(0, -1.8f*Time.deltaTime, 0);
+        Timer = Timer - Time.deltaTime;
+        if(Timer <= 0)
+        {
+            SpeedObject = SpeedObject - 0.1f;
+            Manager.SpeedObjectModificato = SpeedObject;
+            Timer = MaxTimer;
+        }
+
+        transform.position += new Vector3(0, SpeedObject*Time.deltaTime, 0);
     }
     private void OnTriggerEnter2D(Collider2D col)
     {

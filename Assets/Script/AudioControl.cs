beaaf650bd2ec8830de2a536d938ebccc18a2e2d
Giuanix <giovannimascaro88@gmail.com>
2024-07-21 17:21:30 +0200

    AudioMixer

--- Assets/Script/AudioControl.cs
+++ Assets/Script/AudioControl.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 public class AudioControl : MonoBehaviour
 {
     [SerializeField] private Sprite AudioOnImage;
     [SerializeField] private Sprite AudioOffImage;
     [SerializeField] private Button button;
-    [SerializeField] private AudioSource Theme;
+    [SerializeField] private AudioMixer Mixer;
+    private float VolumeMin = -80;
+    private float VolumeMax = 0;
+    private float VolumeAttuale;
     private bool isOn = true;
     // Start is called before the first frame update
     void Start()
     {
-        AudioOnImage = button.image.sprite;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(PlayerPrefs.HasKey("VolGenerale"))
+        {
+            LoadVolume();
+        }
     }
     public void ButtonClicked()
     {
@@ -26,13 +27,34 @@ public class AudioControl : MonoBehaviour
         {
             button.image.sprite = AudioOffImage;
             isOn = false;
-            Theme.mute = true;
+            Mixer.SetFloat("VolGenerale",VolumeMin);
+            VolumeAttuale = VolumeMin;
+            PlayerPrefs.SetFloat("VolGenerale",VolumeAttuale);
         }
         else
         {
             button.image.sprite = AudioOnImage;
             isOn = true;
-            Theme.mute = false;
+            Mixer.SetFloat("VolGenerale",VolumeMax);
+            VolumeAttuale = VolumeMax;
+            PlayerPrefs.SetFloat("VolGenerale",VolumeAttuale);
+        }
+    }
+
+    public void LoadVolume()
+    {
+        VolumeAttuale = PlayerPrefs.GetFloat("VolGenerale");
+        if(VolumeAttuale < 0)
+        {
+            button.image.sprite = AudioOffImage;
+            isOn = false;
+            Mixer.SetFloat("VolGenerale",VolumeMin);
+        }
+        else
+        {
+            button.image.sprite = AudioOnImage;
+            isOn = true;
+            Mixer.SetFloat("VolGenerale",VolumeMax);
         }
     }
 }
